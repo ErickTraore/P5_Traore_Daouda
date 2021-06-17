63b1f94fa4b9b916b185018a30217190c0ca23f9
ErickTraore <traoreerick@gmail.com>
2021-06-17 06:20:40 +0200

    loading project

---
+++ frontend/js/index.js
@@ -0,0 +1,46 @@
+﻿function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function getAllProducts() {
+    // (Ligne 13) Pour comprendre cette boucle https://www.toutjavascript.com/reference/ref-htmlelement.appendchild.php
+    //(Ligne 17) Pour comprendre l'insertion d'image https://www.youtube.com/watch?v=2lt4HmLm8LE&t=525s
+    fetch("http://localhost:3000/api/cameras/")
+        .then(reponse => reponse.json())
+        .then(data => {
+            var p = document.getElementById("articles");
+            for (i = 0; i < data.length; i++) {
+
+                var img = document.createElement("IMG");
+                img.src = data[i].imageUrl;
+                img.classList.add("articles__image");
+                p.appendChild(img);
+
+                var e = document.createElement("a");
+                e.classList.add("articles__article");
+                e.href = "pageProduct.html?id=" + data[i]._id;
+                e.innerHTML = " Détails du produit";
+                p.appendChild(e);
+
+                var e = document.createElement("div");
+                e.classList.add("articles__article");
+                e.innerHTML = "Model:" + " " + data[i].name;
+                p.appendChild(e);
+
+                var e = document.createElement("div");
+                e.classList.add("articles__article");
+                e.innerHTML =
+                    "Prix :" + " " + "<span>" + data[i].price + "." + "</span>";
+                p.appendChild(e);
+            }
+            // .catch(function(err) {
+            //     //Une erreur est survenue
+        });
+
+}
+getAllProducts();
---
+++ frontend/js/products.js
@@ -0,0 +1,50 @@
+﻿function getOneProducts() {
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search.slice(1));
+    let id = location.search.substring(4);
+    alert(id);
+    let apiurl = "http://localhost:3000/api/cameras/" + id;
+    var p = document.getElementById("output");
+    fetch(apiurl)
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(data) {
+            var img = document.createElement("IMG");
+            img.src = data.imageUrl;
+            img.classList.add("output__image");
+            p.appendChild(img);
+
+            var e = document.createElement("div");
+            e.classList.add("output__article");
+            e.innerHTML = "Model:" + " " + data.name;
+            p.appendChild(e);
+
+            var e = document.createElement("div");
+            e.classList.add("output__article");
+            e.innerHTML = "Prix :" + " " + "<span>" + data.price + "." + "</span>";
+            p.appendChild(e);
+
+            var e = document.createElement("div");
+            e.classList.add("output__article");
+            e.innerHTML = "Lentille:" + " " + "<span>" + data.lenses;
+            p.appendChild(e);
+
+            var e = document.createElement("div");
+            e.classList.add("output__article");
+            e.innerHTML =
+                "La qualité de votre produit:" +
+                " " +
+                "<span>" +
+                data.description +
+                "." +
+                "</span>";
+            p.appendChild(e);
+        })
+        .catch(function(err) {
+            // Une erreur est survenue
+        });
+}
+getOneProducts();
